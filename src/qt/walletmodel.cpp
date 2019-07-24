e8acb5ae2ff772ee20fb4e273e65f454a224d98a
furszy <matiasfurszyfer@protonmail.com>
2019-07-24 15:40:32 -0300

    [Model] isTestnet method created, abstracting the UI from backend dependencies.

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -59,6 +59,10 @@ WalletModel::~WalletModel()
     unsubscribeFromCoreSignals();
 }
 
+bool WalletModel::isTestnet() const {
+    return Params().NetworkID() == CBaseChainParams::TESTNET;
+}
+
 CAmount WalletModel::getBalance(const CCoinControl* coinControl) const
 {
     if (coinControl) {
