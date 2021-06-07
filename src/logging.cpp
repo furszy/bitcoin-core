26f9d5a042e49b06cf7c95892be56e1af75a66d0
random-zebra <random.zebra@protonmail.com>
2021-06-07 18:09:30 +0200

    [Utils] Introduce CBatchedLogger
    >>> inspired by dash@9d25bb1d8f0d3e5acc10549d9db8fa52f52be5b2

--- src/logging.cpp
+++ src/logging.cpp
@@ -125,6 +125,7 @@ const CLogCategoryDesc LogCategories[] = {
         {BCLog::VALIDATION,     "validation"},
         {BCLog::LLMQ,           "llmq"},
         {BCLog::NET_MN,         "net_mn"},
+        {BCLog::DKG,            "dkg"},
         {BCLog::ALL,            "1"},
         {BCLog::ALL,            "all"},
 };
@@ -260,4 +261,28 @@ void BCLog::Logger::ShrinkDebugFile()
         }
     } else if (file != NULL)
         fclose(file);
+}
+
+/// PIVX
+
+CBatchedLogger::CBatchedLogger(BCLog::Logger* _logger, BCLog::LogFlags _category, const std::string& _header) :
+    logger(_logger),
+    accept(LogAcceptCategory(_category)),
+    header(_header)
+{}
+
+CBatchedLogger::~CBatchedLogger()
+{
+    Flush();
+}
+
+void CBatchedLogger::Flush()
+{
+    if (!accept || msg.empty()) {
+        return;
+    }
+    if (logger && logger->Enabled()) {
+        logger->LogPrintStr(strprintf("%s:\n%s", header, msg));
+    msg.clear();
+    }
 }
