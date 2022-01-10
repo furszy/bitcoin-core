a613dfca7c5685ded08b7ecbb22862b00578e270
random-zebra <random.zebra@protonmail.com>
2022-01-10 13:52:16 +0100

    RPC: Implement getminedcommitment command

--- src/rpc/rpcquorums.cpp
+++ src/rpc/rpcquorums.cpp
@@ -13,6 +13,35 @@
 #include "rpc/server.h"
 #include "validation.h"
 
+UniValue getminedcommitment(const JSONRPCRequest& request)
+{
+    if (request.fHelp || request.params.size() != 2) {
+        throw std::runtime_error(
+                "getminedcommitment llmq_type quorum_hash\n"
+                "Return information about the commitment for given quorum.\n"
+                "\nArguments:\n"
+                "1. llmq_type         (number, required) LLMQ type.\n"
+                "2. quorum_hash       (hex string, required) LLMQ hash.\n"
+                "\nExamples:\n"
+                + HelpExampleRpc("getminedcommitment", "2 \"xxx\"")
+                + HelpExampleCli("getminedcommitment", "2, \"xxx\"")
+        );
+    }
+
+    Consensus::LLMQType llmq_type = (Consensus::LLMQType) request.params[0].get_int();
+    const uint256& quorum_hash = uint256S(request.params[1].get_str());
+
+    llmq::CFinalCommitment qc;
+    uint256 block_hash;
+    if (!llmq::quorumBlockProcessor->GetMinedCommitment(llmq_type, quorum_hash, qc, block_hash)) {
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "mined commitment not found");
+    }
+
+    UniValue ret(UniValue::VOBJ);
+    qc.ToJson(ret);
+    ret.pushKV("block_hash", block_hash.ToString());
+    return ret;
+}
 
 UniValue quorumdkgstatus(const JSONRPCRequest& request)
 {
@@ -63,6 +92,7 @@ UniValue quorumdkgstatus(const JSONRPCRequest& request)
 static const CRPCCommand commands[] =
 { //  category       name                      actor (function)      okSafe argNames
   //  -------------- ------------------------- --------------------- ------ --------
+    { "evo",         "getminedcommitment",     &getminedcommitment,  true,  {"llmq_type", "quorum_hash"}  },
     { "evo",         "quorumdkgstatus",        &quorumdkgstatus,     true,  {"detail_level"}  },
 };
 
