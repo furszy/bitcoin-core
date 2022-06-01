f55be21b2ebd8b40def171d1b7a7754f1a96a9ae
Andrew Chow <achow101-github@achow101.com>
2022-06-01 12:35:00 -0400

    wallet: Explicitly preserve transaction locktime in CreateTransaction
    We provide the preset nLockTime to CCoinControl so that
    CreateTransactionInternal can be aware of it and set it in the produced
    transaction.

--- src/wallet/spend.cpp
+++ src/wallet/spend.cpp
@@ -980,6 +980,11 @@ static util::Result<CreatedTransactionResult> CreateTransactionInternal(
         }
         txNew.vin.push_back(CTxIn(coin->outpoint, CScript(), sequence.value_or(default_sequence)));
     }
+    if (coin_control.m_locktime) {
+        txNew.nLockTime = coin_control.m_locktime.value();
+        // If we have a locktime set, we can't use anti-fee-sniping
+        use_anti_fee_sniping = false;
+    }
     if (use_anti_fee_sniping) {
         DiscourageFeeSniping(txNew, rng_fast, wallet.chain(), wallet.GetLastBlockHash(), wallet.GetLastBlockHeight());
     }
@@ -1158,6 +1163,9 @@ bool FundTransaction(CWallet& wallet, CMutableTransaction& tx, CAmount& nFeeRet,
         CRecipient recipient = {txOut.scriptPubKey, txOut.nValue, setSubtractFeeFromOutputs.count(idx) == 1};
         vecSend.push_back(recipient);
     }
+
+    // Set the user desired locktime
+    coinControl.m_locktime = tx.nLockTime;
 
     // Acquire the locks to prevent races to the new locked unspents between the
     // CreateTransaction call and LockCoin calls (when lockUnspents is true).
