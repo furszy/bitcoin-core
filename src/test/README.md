c5f5ff3904c30115a70f27bf5b68993d3da804de
Larry Ruane <larryruane@gmail.com>
2022-11-23 17:02:23 -0700

    test: test_bitcoin: allow -testdatadir=<datadir>
    Specifying this argument overrides the path location for test_bitcoin;
    it becomes <datadir>/test_temp. Also, this directory isn't removed
    after the test completes. This can make it easier for developers to study
    the results of a test (see the state of the data directory after the
    test runs), and also (for example) have an editor open on debug.log
    to monitor it across multiple test runs instead of having to re-open a
    different pathname each time.
    
    Example usage (note the "--" is needed):
    
    test_bitcoin --run_test=getarg_tests/boolarg -- \
    -testdatadir=/somewhere/mydatadir
    
    This will create (if necessary) and use the data directory:
    
    /somewhere/mydatadir
    
    Co-authored-by: furszy <mfurszy@protonmail.com>

--- src/test/README.md
+++ src/test/README.md
@@ -41,22 +41,57 @@ test_bitcoin --log_level=all --run_test=getarg_tests
 ```
 
 `log_level` controls the verbosity of the test framework, which logs when a
-test case is entered, for example. `test_bitcoin` also accepts the command
-line arguments accepted by `bitcoind`. Use `--` to separate both types of
-arguments:
+test case is entered, for example.
+
+`test_bitcoin` also accepts some of the command line arguments accepted by
+`bitcoind`. Use `--` to separate these sets of arguments:
 
 ```bash
 test_bitcoin --log_level=all --run_test=getarg_tests -- -printtoconsole=1
 ```
 
-The `-printtoconsole=1` after the two dashes redirects the debug log, which
-would normally go to a file in the test datadir
-(`BasicTestingSetup::m_path_root`), to the standard terminal output.
+The `-printtoconsole=1` after the two dashes sends debug logging, which
+normally goes only to `debug.log` within the data directory, also to the
+standard terminal output.
 
 ... or to run just the doubledash test:
 
 ```bash
 test_bitcoin --run_test=getarg_tests/doubledash
+```
+
+`test_bitcoin` creates a temporary working (data) directory with a randomly
+generated pathname within `test_common_Bitcoin Core/`, which in turn is within
+the system's temporary directory (see
+[`temp_directory_path`](https://en.cppreference.com/w/cpp/filesystem/temp_directory_path)).
+This data directory looks like a simplified form of the standard `bitcoind` data
+directory. Its content will vary depending on the test, but it will always
+have a `debug.log` file, for example.
+
+The location of the temporary data directory can be overridden with the
+`-testdatadir` option, which can make debugging easier. The directory
+path used is the argument path appended with `/test_temp`. The directory
+path is created if necessary.
+Specifying this argument also causes the data directory
+not to be removed after the last test. This is useful for looking at
+what the test wrote to `debug.log` after it completes, for example.
+(The directory is removed at the start of the next test run,
+so no leftover state is used.)
+
+If you run multiple sequential individual tests (for example, `-run_tests=getarg_tests`
+runs a suite of several tests), the data directory will correspond
+only to the _last_ test that runs.
+
+```bash
+$ test_bitcoin --run_test=getarg_tests/doubledash -- -testdatadir=/somewhere/mydatadir
+Test directory (will not be deleted): "/somewhere/mydatadir/test_temp"
+Running 1 test case...
+
+*** No errors detected
+$ ls -l /somewhere/mydatadir
+total 8
+drwxrwxr-x 2 admin admin 4096 Nov 27 22:45 blocks
+-rw-rw-r-- 1 admin admin 1003 Nov 27 22:45 debug.log
 ```
 
 Run `test_bitcoin --help` for the full list.
--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -98,8 +98,7 @@ struct NetworkSetup
 static NetworkSetup g_networksetup_instance;
 
 BasicTestingSetup::BasicTestingSetup(const ChainType chainType, const std::vector<const char*>& extra_args)
-    : m_path_root{fs::temp_directory_path() / "test_common_" PACKAGE_NAME / g_insecure_rand_ctx_temp_path.rand256().ToString()},
-      m_args{}
+    : m_args{}
 {
     m_node.shutdown = &m_interrupt;
     m_node.args = &gArgs;
@@ -120,18 +119,39 @@ BasicTestingSetup::BasicTestingSetup(const ChainType chainType, const std::vecto
         arguments = Cat(arguments, G_TEST_COMMAND_LINE_ARGUMENTS());
     }
     util::ThreadRename("test");
-    fs::create_directories(m_path_root);
-    m_args.ForceSetArg("-datadir", fs::PathToString(m_path_root));
-    gArgs.ForceSetArg("-datadir", fs::PathToString(m_path_root));
     gArgs.ClearPathCache();
     {
         SetupServerArgs(*m_node.args);
+        m_node.args->AddArg("-testdatadir", "Custom data directory (default: <random_string>)", ArgsManager::ALLOW_ANY, OptionsCategory::DEBUG_TEST);
         std::string error;
         if (!m_node.args->ParseParameters(arguments.size(), arguments.data(), error)) {
             m_node.args->ClearArgs();
             throw std::runtime_error{error};
         }
     }
+
+    if (!m_node.args->IsArgSet("-testdatadir")) {
+        // By default, the data directory has a random name
+        const std::string testdatadir{g_insecure_rand_ctx_temp_path.rand256().ToString().c_str()};
+        m_path_root = fs::temp_directory_path() / "test_common_" PACKAGE_NAME / testdatadir;
+    } else {
+        // Custom data directory
+        const std::string testdatadir{m_node.args->GetArg("-testdatadir").value()};
+        m_path_root = fs::path(testdatadir.c_str()) / "test_temp";
+        fs::remove_all(m_path_root);
+
+        // Print the test directory name if custom, but only once per test run
+        static bool g_printed_dir{false};
+        if (!g_printed_dir) {
+            g_printed_dir = true;
+            std::cout << "Test directory (will not be deleted): " << m_path_root << std::endl;
+        }
+        m_has_custom_datadir = true;
+    }
+    fs::create_directories(m_path_root);
+    m_args.ForceSetArg("-datadir", fs::PathToString(m_path_root));
+    gArgs.ForceSetArg("-datadir", fs::PathToString(m_path_root));
+
     SelectParams(chainType);
     SeedInsecureRand();
     if (G_TEST_LOG_FUN) LogInstance().PushBackCallback(G_TEST_LOG_FUN);
@@ -159,7 +179,9 @@ BasicTestingSetup::~BasicTestingSetup()
     m_node.kernel.reset();
     SetMockTime(0s); // Reset mocktime for following tests
     LogInstance().DisconnectTestLogger();
-    fs::remove_all(m_path_root);
+    if (!m_has_custom_datadir) {
+        fs::remove_all(m_path_root);
+    }
     gArgs.ClearArgs();
 }
 
