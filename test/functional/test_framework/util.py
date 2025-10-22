e711a2a5e1e4732960eb8dc7ecae021e38158800
furszy <matiasfurszyfer@protonmail.com>
2025-10-22 08:07:34 -0400

    test: bound number of RPC server threads to 2
    The default `-rpcthreads` value spawns 16 HTTP server threads for each node.
    Running the functional test suite with default `rpcthreads` can exhaust file
    descriptors or hit other resource limits very easily.
    Moreover, having 16 threads is not necessary as they are mostly idle. We
    mostly run every RPC call on a single RPC connection and wait for its result
    synchronously. There is (almost) never two calls occurring concurrently.
    Because of this, the threads are mostly idle, so we can limit the number of them
    to two.

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -444,6 +444,8 @@ def write_config(config_path, *, n, chain, extra_config="", disable_autoconnect=
         # Disable server-side timeouts to avoid intermittent issues
         f.write("rpcservertimeout=99000\n")
         f.write("rpcdoccheck=1\n")
+        # Bound number of RPC server threads
+        f.write("rpcthreads=" + str(2) + "\n")
         f.write("fallbackfee=0.0002\n")
         f.write("server=1\n")
         f.write("keypool=1\n")
