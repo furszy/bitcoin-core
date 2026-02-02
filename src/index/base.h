97a0c617308be745d72f7ed3d13f084d10449bca
furszy <matiasfurszyfer@protonmail.com>
2026-02-02 18:57:54 -0500

    index: add method to process block ranges
    No behavior change.
    
    Introduce ProcessBlocks(start, end) to handle a range of blocks
    in forward order. Currently used per-block, but this lays the
    foundation for future batch processing and parallelization.

--- src/index/base.h
+++ src/index/base.h
@@ -108,6 +108,9 @@ private:
 
     bool ProcessBlock(const CBlockIndex* pindex, const CBlock* block_data = nullptr);
 
+    /// Processes blocks in the range [start, end]. Calling 'ProcessBlock'.
+    bool ProcessBlocks(const CBlockIndex* start, const CBlockIndex* end);
+
     virtual bool AllowPrune() const = 0;
 
     template <typename... Args>
