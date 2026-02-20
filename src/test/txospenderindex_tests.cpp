eee7cf18698e89d1820944b70d75cfa7ca611dfa
furszy <matiasfurszyfer@protonmail.com>
2026-02-20 14:20:43 -0500

    test: index, improve txospenderindex_initial_sync() test case

--- src/test/txospenderindex_tests.cpp
+++ src/test/txospenderindex_tests.cpp
@@ -14,46 +14,56 @@ BOOST_AUTO_TEST_SUITE(txospenderindex_tests)
 
 BOOST_FIXTURE_TEST_CASE(txospenderindex_initial_sync, TestChain100Setup)
 {
-    TxoSpenderIndex txospenderindex(interfaces::MakeChain(m_node), 1 << 20, true);
-    BOOST_REQUIRE(txospenderindex.Init());
-
+    // Setup phase:
     // Mine blocks for coinbase maturity, so we can spend some coinbase outputs in the test.
+    const CScript& coinbase_script = m_coinbase_txns[0]->vout[0].scriptPubKey;
     for (int i = 0; i < 50; i++) {
-        std::vector<CMutableTransaction> no_txns;
-        CreateAndProcessBlock(no_txns, this->m_coinbase_txns[i]->vout[0].scriptPubKey);
+        CreateAndProcessBlock({}, coinbase_script);
     }
+
+    // Spend 10 outputs
     std::vector<COutPoint> spent(10);
     std::vector<CMutableTransaction> spender(spent.size());
-
     for (size_t i = 0; i < spent.size(); i++) {
-        spent[i] = COutPoint(this->m_coinbase_txns[i]->GetHash(), 0);
+        // Outpoint
+        auto output = m_coinbase_txns[i];
+        spent[i] = COutPoint(output->GetHash(), 0);
+
+        // Spending tx
         spender[i].version = 1;
         spender[i].vin.resize(1);
         spender[i].vin[0].prevout.hash = spent[i].hash;
         spender[i].vin[0].prevout.n = spent[i].n;
         spender[i].vout.resize(1);
-        spender[i].vout[0].nValue = this->m_coinbase_txns[i]->GetValueOut();
-        spender[i].vout[0].scriptPubKey = this->m_coinbase_txns[i]->vout[0].scriptPubKey;
+        spender[i].vout[0].nValue = output->GetValueOut();
+        spender[i].vout[0].scriptPubKey = coinbase_script;
 
-        // Sign:
+        // Sign
         std::vector<unsigned char> vchSig;
-        const uint256 hash = SignatureHash(this->m_coinbase_txns[i]->vout[0].scriptPubKey, spender[i], 0, SIGHASH_ALL, 0, SigVersion::BASE);
+        const uint256 hash = SignatureHash(coinbase_script, spender[i], 0, SIGHASH_ALL, 0, SigVersion::BASE);
         coinbaseKey.Sign(hash, vchSig);
         vchSig.push_back((unsigned char)SIGHASH_ALL);
         spender[i].vin[0].scriptSig << vchSig;
     }
 
-    CBlock block = CreateAndProcessBlock(spender, this->m_coinbase_txns[0]->vout[0].scriptPubKey);
+    CBlock block = CreateAndProcessBlock(spender, coinbase_script);
+    m_node.validation_signals->SyncWithValidationInterfaceQueue();
+    uint256 tip_hash = block.GetHash(); // Ensure block has been processed
+    BOOST_CHECK_EQUAL(WITH_LOCK(::cs_main, return m_node.chainman->ActiveTip()->GetBlockHash()), tip_hash);
 
-    // Transaction should not be found in the index before it is started.
+    // Now we concluded the setup phase, run index
+    TxoSpenderIndex txospenderindex(interfaces::MakeChain(m_node), 1 << 20, true);
+    BOOST_REQUIRE(txospenderindex.Init());
+    BOOST_CHECK(!txospenderindex.BlockUntilSyncedToCurrentChain());
+
+    // Transaction should not be found in the index before it is synced.
     for (const auto& outpoint : spent) {
         BOOST_CHECK(!txospenderindex.FindSpender(outpoint).value());
     }
 
-    // BlockUntilSyncedToCurrentChain should return false before txospenderindex is started.
-    BOOST_CHECK(!txospenderindex.BlockUntilSyncedToCurrentChain());
-
     txospenderindex.Sync();
+    BOOST_CHECK_EQUAL(txospenderindex.GetSummary().best_block_hash, tip_hash);
+
     for (size_t i = 0; i < spent.size(); i++) {
         const auto tx_spender{txospenderindex.FindSpender(spent[i])};
         BOOST_REQUIRE(tx_spender.has_value());
@@ -62,15 +72,6 @@ BOOST_FIXTURE_TEST_CASE(txospenderindex_initial_sync, TestChain100Setup)
         BOOST_CHECK_EQUAL((*tx_spender)->block_hash, block.GetHash());
     }
 
-    // It is not safe to stop and destroy the index until it finishes handling
-    // the last BlockConnected notification. The BlockUntilSyncedToCurrentChain()
-    // call above is sufficient to ensure this, but the
-    // SyncWithValidationInterfaceQueue() call below is also needed to ensure
-    // TSAN always sees the test thread waiting for the notification thread, and
-    // avoid potential false positive reports.
-    m_node.validation_signals->SyncWithValidationInterfaceQueue();
-
-    // shutdown sequence (c.f. Shutdown() in init.cpp)
     txospenderindex.Stop();
 }
 
